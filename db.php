f21de2f0a2f0d56c2cde79871cb4d1f352cfe6e7
OualidHamdi <oualidhamdi12@gmail.com>
2021-02-03 23:52:09 +0100

    LIBRARY DB CONFIG

--- db.php
+++ db.php
@@ -1,19 +1,18 @@
-
 <?php
-session_start();
-header('Cache-Control: no-cache');
-$serveur_name ="localhost";
-	$db_username ="root";
-	$db_password = "";
-	$db_name="library";
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-	$connection = mysqli_connect($serveur_name,$db_username,$db_password);
-	$connection -> set_charset("utf8");
-	$dbconfig = mysqli_select_db($connection,$db_name);
-    
 
 
-	if (!$dbconfig) {
+	if (!$conn) {
     die("La connexion a échoué: " . mysqli_connect_error()  . "<br/>" );
     
 }
