7f810b1e050beceb8732f26993659d52816a9646
OualidHamdi <oualidhamdi12@gmail.com>
2021-02-03 23:55:13 +0100

    LIBRARY DB CONFIG

--- db.php
+++ db.php
@@ -8,11 +8,11 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 
-	if (!$conn) {
+	if (!$connection) {
     die("La connexion a échoué: " . mysqli_connect_error()  . "<br/>" );
     
 }
