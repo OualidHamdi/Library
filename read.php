503482e7803676b706482cf78955655a58617e7b
OualidHamdi <oualidhamdi12@gmail.com>
2021-02-04 21:24:34 +0100

    style button

--- read.php
+++ read.php
@@ -10,13 +10,13 @@
             <td><?php echo '<img src="upload/'.$row['image'].'" width="100">'?></td>
             <td><?php echo $row['publishedat']; ?></td>
             <td>
-              <form action="updateBook.php" method="post">
+              <form action="updateBook.php" method="post" style="float: left;">
                 <input type="hidden" name="edit_id" value="<?php echo $row['ID']; ?>">
                <button  type="submit"  name="edit_Book" class="btn btn-warning"> Update</button>
               </form>
       
               
-                   <form action="deleteBook.php" method="post">
+                   <form action="deleteBook.php" method="post" >
                      <input type="hidden" name="delete_id" value="<?php echo $row['ID']; ?>">
                       <button type="submit" name="delete_btn" class="btn btn-danger" > Delete</button>
                    </form>
