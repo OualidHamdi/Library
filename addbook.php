62fc04c3f53d492999534878da01148dbbd80cc3
OualidHamdi <oualidhamdi12@gmail.com>
2021-02-04 19:23:46 +0100

    separer CSS

--- addbook.php
+++ addbook.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link rel="stylesheet" href="./addbooks.css">
+    <link rel="stylesheet" href="style/addbooks.css">
     <title>Library</title>
 </head>
 <body>
