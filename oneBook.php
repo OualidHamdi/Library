2e0797d42e236acefea4e27ceabf95a539119538
OualidHamdi <oualidhamdi12@gmail.com>
2021-02-09 22:07:39 +0100

    Client Side

--- oneBook.php
+++ oneBook.php
@@ -1,8 +1,8 @@
 
 <!DOCTYPE html>
-<base href="http://localhost/library/onebook.php">
 <html lang="en">
 <head>
+  <base href="http://localhost/library/onebook.php">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include('function/boostrap.php') ?>
